668b18a6147da08cbdedb5ab6f1245ef13eb6e56
Jutta <jutta.frieden@gmail.com>
2014-08-28 11:20:02 +0100

    Added definition of success and completed first fully functionable version of the game

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,4 @@
-# Define global variables for the 9 fields we have in TicTacToe
+# Define global variables for the 9 fields we have in TicTacToe and $w for the winner boolean variable
 
 $p1=' '
 $p2=' '
@@ -9,17 +9,32 @@ $p6=' '
 $p7=' '
 $p8=' '
 $p9=' '
+$w=false
 
 # An array which will keep count of our global variables
 
-$a=[0, $p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
+$a=[0,$p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
 
 # Our main function which will run the game
 
 def main
     draw_board
-    player_1_input
-    player_2_input
+    while $w==false && $a.any? {|e| e == ' '}
+        player_1_input
+        winner1
+        if $w==true
+            break
+        end
+        if $a.all? { |e| e!=' '}
+            break
+        end
+        player_2_input
+        winner2
+    end
+    if $w==false 
+        puts "Even! There is no winner this time. Why don't you play again?"
+    end
+
 end
 
 # Function to draw the board to the terminal
@@ -38,8 +53,12 @@ def player_1_input
     print "Player 1, it's your turn:"
     input = gets.chomp.to_i
 
-    while input<1 || input>9
-        puts "You need to put a number between 1 and 9, try again:"
+     while input<1 || input>9 || $a[input] != ' '
+        if input<1 || input>9
+            print "You need to put a number between 1 and 9, try again:"
+        else
+            print "Sorry, this field is already taken, please choose a different field:"
+        end
         input = gets.chomp.to_i
     end
 
@@ -64,9 +83,18 @@ def player_1_input
         $p9='X'
     end
 
-    $a=[0, $p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
+    $a=[0,$p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
 
     draw_board
+end
+
+# Check whether player 1 has won
+
+def winner1
+    if [[$p1,$p2,$p3],[$p4,$p5,$p6],[$p7,$p8,$p9],[$p1,$p4,$p7],[$p2,$p5,$p8],[$p3,$p6,$p9],[$p1,$p5,$p9],[$p3,$p5,$p7]].any? { |e| e==['X','X','X']} 
+        puts "Congratulations, player 1 has won!"
+        $w=true
+    end
 end
 
 # Function to get input by player 2
@@ -75,13 +103,17 @@ def player_2_input
     print "Player 2, it's your turn:"
     input = gets.chomp.to_i
 
-    while input<1 || input>9
-        puts "You need to put a number between 1 and 9, try again:"
-        input = gets.chomp.to_i
-    end
+    # while input<1 || input>9
+    #     puts "You need to put a number between 1 and 9, try again:"
+    #     input = gets.chomp.to_i
+    # end
 
-    while $a[input] != ' '
-        print "Sorry, this field is already taken, please choose a different field:"
+    while input<1 || input>9 || $a[input] != ' '
+        if input<1 || input>9
+            print "You need to put a number between 1 and 9, try again:"
+        else
+            print "Sorry, this field is already taken, please choose a different field:"
+        end
         input = gets.chomp.to_i
     end
 
@@ -106,10 +138,18 @@ def player_2_input
         $p9='O'
     end
     
-    $a=[0, $p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
+    $a=[0,$p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9]
     
     draw_board
 end
 
+# Check whether player 2 has won
+
+def winner2
+    if  [[$p1,$p2,$p3],[$p4,$p5,$p6],[$p7,$p8,$p9],[$p1,$p4,$p7],[$p2,$p5,$p8],[$p3,$p6,$p9],[$p1,$p5,$p9],[$p3,$p5,$p7]].any? { |e| e==['O','O','O']} 
+        puts "Congratulations, player 2 has won!"
+        $w=true   
+    end
+end
 
 main
